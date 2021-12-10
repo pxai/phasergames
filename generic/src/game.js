7a7ef7aaecb2b1eee39eff24053062545f1861d7
pxai <pello.altadill@gluckgames.com>
2021-12-10 23:28:52 +0100

    Added generic

---
+++ generic/src/game.js
@@ -0,0 +1,69 @@
+export default class Game extends Phaser.Scene {
+    constructor () {
+        super({ key: "game" });
+        this.player = null;
+        this.score = 0;
+        this.scoreText = null;
+    }
+
+    init (data) {
+      this.name = data.name;
+      this.number = data.number;
+      this.time = data.time;
+  }
+
+    preload () {
+    }
+
+    create () {
+      this.duration = this.time * 1000;
+      this.width = this.sys.game.config.width;
+      this.height = this.sys.game.config.height;
+      this.center_width = this.width / 2;
+      this.center_height = this.height / 2;
+      
+
+      //this.loadAudios(); 
+      // this.playMusic();
+    }
+
+      loadAudios () {
+        this.audios = {
+          "beam": this.sound.add("beam"),
+        };
+      }
+
+      playAudio(key) {
+        this.audios[key].play();
+      }
+
+      playMusic (theme="game") {
+        this.theme = this.sound.add(theme);
+        this.theme.stop();
+        this.theme.play({
+          mute: false,
+          volume: 1,
+          rate: 1,
+          detune: 0,
+          seek: 0,
+          loop: true,
+          delay: 0
+      })
+      }
+
+    update() {
+
+    }
+
+    finishScene () {
+      this.sky.stop();
+      this.theme.stop();
+      this.scene.start("transition", {next: "underwater", name: "STAGE", number: this.number + 1, time: this.time * 2});
+    }
+
+    updateScore (points = 0) {
+        const score = +this.registry.get("score") + points;
+        this.registry.set("score", score);
+        this.scoreText.setText(Number(score).toLocaleString());
+    }
+}
