ddbf510a0d7bbc11b9b5cc2405fdd7f135f823b6
pxai <pello.altadill@gmail.com>
2022-06-26 14:26:29 +0200

    Johnny depth

---
+++ johnny_depth/src/bubble.js
@@ -0,0 +1,29 @@
+class Bubble extends Phaser.GameObjects.Sprite {
+    constructor (scene, x, y, velocity = 1, direction) {
+        super(scene, x, y, "bubble");
+        this.name = "bubble";
+        this.scene = scene;
+        this.setOrigin(0.5)
+
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+        this.body.setAllowGravity(false);
+        this.direction = direction || Phaser.Math.Between(0, 1) ? -1 : 1;
+        this.body.setVelocityY(this.direction * velocity);
+        this.flipX = this.direction < 0;
+        this.init();
+    }
+
+    init () {
+        this.scene.tweens.add({
+            targets: this,
+            duration: Phaser.Math.Between(600, 1000),
+            y: {from: this.y, to: this.y + (this.direction * Phaser.Math.Between(20, 40))},
+            scale: {from: Phaser.Math.Between(0.8, 1.2), to: 0.1},
+            alpha: { from: 1, to: 0 },
+            onComplete: () => { this.destroy() }
+        });
+    }
+}
+
+export default Bubble;
---
+++ johnny_depth/src/fish.js
@@ -0,0 +1,59 @@
+export default class Fish extends Phaser.Physics.Arcade.Sprite {
+    constructor (scene, x, y, type="right") {
+        super(scene, x, y, "fish");
+        this.name = "fish";
+        this.scene = scene;
+        this.scene.physics.add.existing(this);
+        this.scene.physics.world.enable(this);
+        this.body.setAllowGravity(false);
+        this.scene.add.existing(this);
+        this.direction = type === "right" ? 1 : -1;
+
+        this.init();
+    }
+
+    init () {
+        this.scene.anims.create({
+            key: this.name,
+            frames: this.scene.anims.generateFrameNumbers(this.name, { start: 0, end: 0 }),
+            frameRate: 5,
+            repeat: -1
+          });
+
+          this.scene.anims.create({
+            key: this.name + "death",
+            frames: this.scene.anims.generateFrameNumbers(this.name, { start: 0, end: 0 }),
+            frameRate: 5,
+            repeat: -1
+          });
+  
+          this.anims.play(this.name, true)
+          this.body.setVelocityX(this.direction * 150);
+          this.flipX = this.direction > 0;
+          this.on('animationcomplete', this.animationComplete, this);
+    }
+
+
+    update () {
+    }
+
+    turn () {
+        this.direction = -this.direction;
+        this.flipX = this.direction > 0;
+        this.body.setVelocityX(this.direction * 150);
+    }
+
+    death () {
+        this.dead = true;
+        this.body.enable = false;
+        this.body.rotation = 0;
+        this.anims.play(this.name + "death")
+      }
+
+      animationComplete(animation, frame) {
+        if (animation.key === "death") {
+          this.destroy()
+        }
+    }
+}
+
