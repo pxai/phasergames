9b6ce6b2cee6846bb54502ec74843b77660514b9
pxai <pello.altadill@gmail.com>
2023-10-29 08:54:45 +0100

    Added latest

---
+++ project_templates/basic/GameScene.js
@@ -0,0 +1,17 @@
+class GameScene extends Phaser.Scene {
+    constructor () {
+        super({ key: "GameScene"});
+    }
+
+    preload () {
+        console.log("Preload");
+    }
+
+    create () {
+        console.log("Create");
+    }
+
+    update (time, delta) {
+        //console.log("Update", time, delta);
+    }
+}
---
+++ project_templates/basic/init.js
@@ -0,0 +1,9 @@
+const config = {
+    width: 400,
+    height: 300,
+    parent: "container",
+    type: Phaser.CANVAS,
+    scene: [GameScene]
+};
+
+new Phaser.Game(config);
