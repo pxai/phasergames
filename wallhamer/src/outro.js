a470089f4f6de1ab448f3dbea2e1d77d4b2be776
pxai <pello.altadill@gmail.com>
2024-03-16 12:17:52 +0100

    Formatted wallhammer

--- wallhamer/src/outro.js
+++ wallhamer/src/outro.js
@@ -1,50 +1,60 @@
 export default class Outro extends Phaser.Scene {
-    constructor () {
-        super({ key: "outro" });
-    }
+  constructor() {
+    super({ key: "outro" });
+  }
 
-    /*
+  /*
     This scene will show some text
     */
-    create () {
-        this.width = this.sys.game.config.width;
-        this.height = this.sys.game.config.height;
-        this.center_width = this.width / 2;
-        this.center_height = this.height / 2;
-        this.introLayer = this.add.layer();
-        this.splashLayer = this.add.layer();
-        this.text = [
-            "You did it!!",
-            "Thanks to your building skills",
-            "and your mighty hammer,",
-            "you saved the earth.",
-            "Made in 3 days for Minijam\n by Pello\n\n Press SPACE"
-        ];
-        this.showHistory();
+  create() {
+    this.width = this.sys.game.config.width;
+    this.height = this.sys.game.config.height;
+    this.center_width = this.width / 2;
+    this.center_height = this.height / 2;
+    this.introLayer = this.add.layer();
+    this.splashLayer = this.add.layer();
+    this.text = [
+      "You did it!!",
+      "Thanks to your building skills",
+      "and your mighty hammer,",
+      "you saved the earth.",
+      "Made in 3 days for Minijam\n by Pello\n\n Press SPACE",
+    ];
+    this.showHistory();
 
-        this.input.keyboard.on("keydown-SPACE", this.startSplash, this);
-        this.input.keyboard.on("keydown-ENTER", this.startSplash, this);
-    }
+    this.input.keyboard.on("keydown-SPACE", this.startSplash, this);
+    this.input.keyboard.on("keydown-ENTER", this.startSplash, this);
+  }
 
-    startSplash () {
-        this.scene.start("splash");
-    }
+  startSplash() {
+    this.scene.start("splash");
+  }
 
-    /*
+  /*
     Helper function to show the text line by line
     */
-    showHistory () {
-        this.text.forEach((line, i) => {
-                this.time.delayedCall((i + 1) * 2000, () => this.showLine(line, (i + 1) * 60), null, this);
-        });
-    }
+  showHistory() {
+    this.text.forEach((line, i) => {
+      this.time.delayedCall(
+        (i + 1) * 2000,
+        () => this.showLine(line, (i + 1) * 60),
+        null,
+        this
+      );
+    });
+  }
 
-    showLine(text, y) {
-        let line = this.introLayer.add(this.add.bitmapText(this.center_width, y, "pixelFont", text, 25).setOrigin(0.5).setAlpha(0));
-        this.tweens.add({
-            targets: line,
-            duration: 2000,
-            alpha: 1
-        })
-    }
+  showLine(text, y) {
+    let line = this.introLayer.add(
+      this.add
+        .bitmapText(this.center_width, y, "pixelFont", text, 25)
+        .setOrigin(0.5)
+        .setAlpha(0)
+    );
+    this.tweens.add({
+      targets: line,
+      duration: 2000,
+      alpha: 1,
+    });
+  }
 }
--- wallhamer/src/particle.js
+++ wallhamer/src/particle.js
@@ -1,27 +1,27 @@
-
 /*
 This class is used to create the smoke particles. It's a simple rectangle that scales down and fades out.
 */
 export class Smoke extends Phaser.GameObjects.Rectangle {
-    constructor (scene, x, y, width, height, color = 0xffffff, gravity = false ) {
-        width = width || Phaser.Math.Between(10, 25)
-        height = height || Phaser.Math.Between(10, 25)
-        super(scene, x, y, width, height, color)
-        scene.add.existing(this)
-        this.scene = scene;
-        this.color = color;
-        this.init();
-    }
+  constructor(scene, x, y, width, height, color = 0xffffff, gravity = false) {
+    width = width || Phaser.Math.Between(10, 25);
+    height = height || Phaser.Math.Between(10, 25);
+    super(scene, x, y, width, height, color);
+    scene.add.existing(this);
+    this.scene = scene;
+    this.color = color;
+    this.init();
+  }
 
-    init () {
-        this.scene.tweens.add({
-            targets: this,
-            duration: 800,
-            scale: {from: 1, to: 0},
-            onComplete: () => { this.destroy()  }
-        });
-
-    }
+  init() {
+    this.scene.tweens.add({
+      targets: this,
+      duration: 800,
+      scale: { from: 1, to: 0 },
+      onComplete: () => {
+        this.destroy();
+      },
+    });
+  }
 }
 
 // 0xa13000 red brick
@@ -33,67 +33,68 @@ export class Smoke extends Phaser.GameObjects.Rectangle {
 This is similar to the previous one but it represents smoke of rock that we will generate when the player breaks something.
 */
 export class RockSmoke extends Phaser.GameObjects.Rectangle {
-    constructor (scene, x, y, width, height, color = 0xFFEAAB, gravity = false ) {
-        width = width || Phaser.Math.Between(30, 55)
-        height = height || Phaser.Math.Between(30, 55)
-        super(scene, x, y, width, height, color)
-        scene.add.existing(this)
-        scene.physics.add.existing(this);
-        this.body.setAllowGravity(false);
-        this.body.setVelocityY(-100);
-        this.init();
-    }
+  constructor(scene, x, y, width, height, color = 0xffeaab, gravity = false) {
+    width = width || Phaser.Math.Between(30, 55);
+    height = height || Phaser.Math.Between(30, 55);
+    super(scene, x, y, width, height, color);
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+    this.body.setAllowGravity(false);
+    this.body.setVelocityY(-100);
+    this.init();
+  }
 
-    init () {
-        this.scene.tweens.add({
-            targets: this,
-            duration: 800,
-            scale: {from: 1, to: 0},
-            onComplete: () => { this.destroy()  }
-        });
-
-    }
+  init() {
+    this.scene.tweens.add({
+      targets: this,
+      duration: 800,
+      scale: { from: 1, to: 0 },
+      onComplete: () => {
+        this.destroy();
+      },
+    });
+  }
 }
 
 /*
 This is similar to the smoke, but it represents the smoke that comes out when the player jumps and it has gravity.
 */
 export class JumpSmoke extends Phaser.GameObjects.Rectangle {
-    constructor (scene, x, y, width, height, color = 0xFFEAAB, gravity = false ) {
-        width = width || Phaser.Math.Between(10, 25)
-        height = height || Phaser.Math.Between(10, 25)
-        super(scene, x, y, width, height, color)
-        scene.add.existing(this)
-        scene.physics.add.existing(this);
-        this.body.setAllowGravity(false);
-        this.body.setVelocityX(Phaser.Math.Between(-20, 20));
-        this.init();
-    }
+  constructor(scene, x, y, width, height, color = 0xffeaab, gravity = false) {
+    width = width || Phaser.Math.Between(10, 25);
+    height = height || Phaser.Math.Between(10, 25);
+    super(scene, x, y, width, height, color);
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+    this.body.setAllowGravity(false);
+    this.body.setVelocityX(Phaser.Math.Between(-20, 20));
+    this.init();
+  }
 
-    init () {
-        this.scene.tweens.add({
-            targets: this,
-            duration: 800,
-            scale: {from: 1, to: 0},
-            onComplete: () => { this.destroy()  }
-        });
-
-    }
+  init() {
+    this.scene.tweens.add({
+      targets: this,
+      duration: 800,
+      scale: { from: 1, to: 0 },
+      onComplete: () => {
+        this.destroy();
+      },
+    });
+  }
 }
 
 /*
 This represents pieces of rock that we will generate when the player breaks something.
 */
 export class Debris extends Phaser.GameObjects.Rectangle {
-    constructor (scene, x, y, color = 0xb03e00,  width, height, gravity = false ) {
-        width = width || Phaser.Math.Between(15, 30)
-        height = height || Phaser.Math.Between(15, 30)
-        super(scene, x, y + 5, width, height, color)
-        scene.add.existing(this)
-        scene.physics.add.existing(this);
-        this.body.setAllowGravity(true);
-        this.body.setVelocityX(Phaser.Math.Between(-50, 50));
-        this.body.setVelocityY(width * height);
-    }
+  constructor(scene, x, y, color = 0xb03e00, width, height, gravity = false) {
+    width = width || Phaser.Math.Between(15, 30);
+    height = height || Phaser.Math.Between(15, 30);
+    super(scene, x, y + 5, width, height, color);
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+    this.body.setAllowGravity(true);
+    this.body.setVelocityX(Phaser.Math.Between(-50, 50));
+    this.body.setVelocityY(width * height);
+  }
 }
-
