6921cfae80faaacbc35f6e7aa7cbba018f3a998c
pxai <pello.altadill@gmail.com>
2024-03-17 09:03:20 +0100

    Fixed push pull

--- pushpull/src/init.js
+++ pushpull/src/init.js
@@ -6,30 +6,23 @@ import Transition from "./transition";
 import Game from "./game";
 
 const config = {
-    width: 608,
-    height: 608,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+  width: 608,
+  height: 608,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  autoRound: false,
+  parent: "contenedor",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
     },
-    autoRound: false,
-    parent: "contenedor",
-    physics: {
-        default: "arcade",
-        arcade: {
-          gravity: { y: 0 },
-          debug: false
-      }
-    },
-    plugins: {
-      },
-    scene: [
-        Bootloader,
-        Splash,
-        Transition,
-        Game,
-        Outro,
-    ]
+  },
+  plugins: {},
+  scene: [Bootloader, Splash, Transition, Game, Outro],
 };
 
 const game = new Phaser.Game(config);
